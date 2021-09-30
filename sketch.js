e120eb606237772a454d990d36c788a7096a1b20
Watermelon207 <88516062+Watermelon207@users.noreply.github.com>
2021-09-30 19:03:50 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+
+function preload()
+{
+	var ball_options={
+		isStatic:false,
+		restitution:0.3
+	}
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
